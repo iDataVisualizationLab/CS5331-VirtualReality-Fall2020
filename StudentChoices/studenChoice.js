7d515867160099bacbb03db3848ba6ddda393c6d
Zipexpo <nvtngan@hcmus.edu.vn>
2020-08-30 13:50:30 -0500

    update

--- StudentChoices/studenChoice.js
+++ StudentChoices/studenChoice.js
@@ -3,12 +3,14 @@ let key = ['Timestamp','Your name','Your topic','Professor']
 // let key = ['Your name','Your topic','Note','Scheduled','Link','Report']
 d3.csv('CS4331 and CS5331_ Student choice (Responses) - Form Responses 1.csv').then(function(dataRaw){
     let data = dataRaw;
-    let interested_level = d3.scaleOrdinal(d3.schemeBlues);
+    let interested_level = d3.scaleLinear().domain([0,4]) .range(["white", "#6ece58"]);
+
     d3.select('#currentTopic tbody').selectAll('tr').data(data)
         .join('tr')
-        .style('background-color',d=>d['Professor']==='Approve'?(d['Interested level']?interested_level(d['Interested level']):null):'#ffc4c4')
+        .style('background-color',d=>d['Professor']==='Approve'?interested_level(d['Interested level']):'#ffc4c4')
         .selectAll('td')
         .data(d=>key.map(k=>d[k]))
         .join('td')
-        .text(d=>d)
+        .text(d=>d);
+
 });
