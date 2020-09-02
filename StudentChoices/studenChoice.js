388b79bb8c87dfbc88be76f724cad57c15c99cc7
Tommy Dang <tommydang@nvtngan.local>
2020-09-02 11:23:36 -0500

    add syllabus

--- StudentChoices/studenChoice.js
+++ StudentChoices/studenChoice.js
@@ -1,5 +1,5 @@
 lastTime = new Date('8/30/2020');
-let key = ['ID','Timestamp','Your name','Your topic','Schedual','Image'];
+let key = ['ID','Timestamp','Your name','Your topic','Schedule','Image'];
 Promise.all([d3.csv('../grade/data/Students.csv')
 ,d3.csv('CS4331 and CS5331_ Student choice (Responses) - Form Responses 1.csv')])
 .then(function(dataRaw){
@@ -10,7 +10,7 @@ Promise.all([d3.csv('../grade/data/Students.csv')
     limit_time.setSeconds(0);
 
     let data = dataRaw[1];
-    let notPresentyet = data.filter(d=>(d.date=new Date(d['Schedual']+'/2020'), d.date>limit_time));
+    let notPresentyet = data.filter(d=>(d.date=new Date(d['Schedule']+'/2020'), d.date>limit_time));
     let thresholdDate = new Date(+d3.min(notPresentyet,d=>d.date));
     thresholdDate.setHours(16);
     notPresentyet.filter(d=>d.date<thresholdDate).forEach(d=>d.isHighlight = true);
